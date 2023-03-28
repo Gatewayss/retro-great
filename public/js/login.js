aab5ca7c9795b7068c790959b2813afd80064705
Sarah Richens <sarahgrichens@gmail.com>
2023-03-27 19:41:30 -0500

    add birthdate requirement

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ const loginFormHandler = async (event) => {
     } else {
       alert(response.statusText);
     }
-  }
+  } else {Swal.fire("Please enter a valid email and password")}
 };
 
 const signupFormHandler = async (event) => {
@@ -25,20 +25,33 @@ const signupFormHandler = async (event) => {
   const name = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+  const entered_birthdate = document.querySelector('#birthdate-signup').value;
 
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+  let dob = new Date(entered_birthdate);
+  let month_diff = Date.now() - dob.getTime();
+  let age_dt = new Date(month_diff);
+  let year = age_dt.getUTCFullYear();
+  let age = Math.abs(year - 1970);
+  let ageDiff = 48 - age;
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
-    }
-  }
+  console.log("Age of the person: "+ age + " years");
+
+  if(age >= 48) {let birth_date = entered_birthdate;
+    if (name && email && password && birth_date && age) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password, birth_date, age }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        Swal.fire(response.statusText);
+      }
+     } else { Swal.fire("Please make sure to enter your name, a valid email, a secure password and your birthdate")}  
+  } else if( age < 48) 
+    Swal.fire("Sorry - you are not old enough. Please come back in " + ageDiff + " years. See you then!");  
 };
 
 document
