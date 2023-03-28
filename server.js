f0a2a3e579c258e50b3dd00a4797e0dfd56c3800
Gage Norton <nortongage0@gmail.com>
2023-03-27 20:38:13 -0500

    Fix server bug

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const routes = require('./controllers');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
@@ -27,26 +30,26 @@ const sess = {
   })
 };
 
-io.on('connection', (socket) => {
-  // welcome message
-  socket.emit('message', 'Welcome to the Retro Great chat room! Click which room you would like to chat in or send a message to everyone here! :)')
+// io.on('connection', (socket) => {
+//   // welcome message
+//   socket.emit('message', 'Welcome to the Retro Great chat room! Click which room you would like to chat in or send a message to everyone here! :)')
 
-  // broadcast when user connects 
-  socket.broadcast.emit('message', 'A user has joined the chat!');
+//   // broadcast when user connects 
+//   socket.broadcast.emit('message', 'A user has joined the chat!');
 
-  // listen for chat message
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
+//   // listen for chat message
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+//     io.emit('chat message', msg);
+//   });
 
-  // join room and message
-  socket.on("join-room", (room, cb) => {
-    socket.join(room)
-    cb(`Joined ${room}`)
-  });
+//   // join room and message
+//   socket.on("join-room", (room, cb) => {
+//     socket.join(room)
+//     cb(`Joined ${room}`)
+//   });
  
-});
+// });
 
 app.use(cors());
 app.use(session(sess));
