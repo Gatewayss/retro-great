b61142e98ccd020677961112fa7c441e7ececf71
Sarah Richens <sarahgrichens@gmail.com>
2023-03-27 19:31:35 -0500

    finish date format

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['name'],          
         },
       ],
     });
